517dd508e82e7530f0d00da5a435156210a0c585
Vaughn Fajardo <40432972+vaughnfajardo@users.noreply.github.com>
2022-10-05 12:41:44 +0800

    initial push to git

---
+++ main.py
@@ -0,0 +1,35 @@
+{"intents": [
+        {"tag": "greeting",
+         "patterns": ["Hi", "Is anyone there?", "Hello", "Good day", "Good afternoon", "Good evening", "Still open?", "Still available?"],
+         "responses": ["Welcome to SKS Korean Samgyupsal! How can we help you get started? We are open from 10 AM to 10 PM.", "Hello! SKS is open and ready to serve you. We are open from 10 AM to 10 PM. How can we help you get started?"],
+         "context_set": ""
+        },
+        {"tag": "location",
+         "patterns": ["Location?", "Where are you located?", "Where are you from?", "Where will the item be originating from?", "Where is the pick-up point?"],
+         "responses": ["We have branches across Metro Manila in Tondo and Tomas Morato, Laguna, and Cavite."],
+         "context_set": ""
+        },
+        {"tag": "contact",
+         "patterns": ["Can we call?", "What is your contact number?", "Cellphone number?", "Mobile number?", "What are your contact details?"],
+         "responses": ["To process orders "],
+         "context_set": ""
+        },
+
+   ]
+}
+
+
+
+what is your contact numbers
+what are your branches
+reservation? no physical place. delivery only
+do you have dine in and take out? (delivery only)
+what is the menu
+what do you recommend for good for how many ax
+what is the best seller
+do you have freebies
+what is mode of payment
+how much is the cost of this package
+what is the delivery fee
+mode of delivery (grab, lalamove, own rider)
+thank you! - send feedback and recommendation
